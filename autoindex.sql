0fbf63cc94c6110c872c3c0b4f9d075883a5eecc
Vivek Sharma <viveksh29@gmail.com>
2025-07-08 11:50:56 -0400

    Update autoindex.sql

--- autoindex.sql
+++ autoindex.sql
@@ -30,3 +30,69 @@ FROM (
 ) AS Scripts;
 
 PRINT @SQL;
+
+
+
+DECLARE @TableName SYSNAME = 'YourTableName';
+DECLARE @SchemaName SYSNAME = 'dbo';
+
+DECLARE @SQL NVARCHAR(MAX) = '';
+
+-- Cursor to iterate through indexes
+DECLARE IndexCursor CURSOR FOR
+SELECT i.name, i.is_unique, i.type_desc, i.index_id
+FROM sys.indexes i
+WHERE i.object_id = OBJECT_ID(QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName))
+  AND i.is_primary_key = 0
+  AND i.type_desc IN ('CLUSTERED', 'NONCLUSTERED');
+
+OPEN IndexCursor;
+
+DECLARE @IndexName SYSNAME, @IsUnique BIT, @TypeDesc NVARCHAR(60), @IndexId INT;
+
+FETCH NEXT FROM IndexCursor INTO @IndexName, @IsUnique, @TypeDesc, @IndexId;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    DECLARE @IndexCols NVARCHAR(MAX) = '';
+    DECLARE @IncludeCols NVARCHAR(MAX) = '';
+
+    -- Get key columns
+    SELECT @IndexCols = STUFF((
+        SELECT ', ' + QUOTENAME(c.name) + 
+               CASE ic.is_descending_key WHEN 1 THEN ' DESC' ELSE ' ASC' END
+        FROM sys.index_columns ic
+        JOIN sys.columns c 
+          ON ic.object_id = c.object_id AND ic.column_id = c.column_id
+        WHERE ic.object_id = OBJECT_ID(QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName))
+          AND ic.index_id = @IndexId
+          AND ic.is_included_column = 0
+        ORDER BY ic.key_ordinal
+        FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 2, '');
+
+    -- Get included columns
+    SELECT @IncludeCols = STUFF((
+        SELECT ', ' + QUOTENAME(c.name)
+        FROM sys.index_columns ic
+        JOIN sys.columns c 
+          ON ic.object_id = c.object_id AND ic.column_id = c.column_id
+        WHERE ic.object_id = OBJECT_ID(QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName))
+          AND ic.index_id = @IndexId
+          AND ic.is_included_column = 1
+        ORDER BY ic.index_column_id
+        FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 2, '');
+
+    SET @SQL += 'CREATE ' + 
+                CASE WHEN @IsUnique = 1 THEN 'UNIQUE ' ELSE '' END + 
+                @TypeDesc + ' INDEX [' + @IndexName + '] ON [' + @SchemaName + '].[' + @TableName + '] (' + 
+                @IndexCols + ')' + 
+                CASE WHEN @IncludeCols IS NOT NULL THEN ' INCLUDE (' + @IncludeCols + ')' ELSE '' END +
+                ';' + CHAR(13) + CHAR(10);
+
+    FETCH NEXT FROM IndexCursor INTO @IndexName, @IsUnique, @TypeDesc, @IndexId;
+END
+
+CLOSE IndexCursor;
+DEALLOCATE IndexCursor;
+
+PRINT @SQL;
